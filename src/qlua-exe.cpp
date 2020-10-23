bcd5f09da0d9ad0679ecdd76eaa4595f96e738ab
swerg <swerg@swerg.ru>
2020-10-23 22:21:47 +0700

    It works!

---
+++ src/qlua-exe.cpp
@@ -0,0 +1,49 @@
+//
+// https://github.com/swerg/qlua-exe
+//
+
+#include <iostream>
+
+#include <windows.h>
+#include <process.h>
+
+#include "lua.hpp"
+
+
+int main(int argc, char ** argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "err: File is required as argument" << std::endl
+			<< "Usage:" << std::endl
+			<< "   qlua.exe <filename.lua> {<arg1> <arg2> ...}" << std::endl;
+		return 2;
+	}
+
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
+	int error = luaL_loadfile(L, argv[1]);
+	if (!error)
+	{
+		// pass lua-file name and extra arguments into a global 'arg' table
+		lua_newtable(L);
+		for (int i = 1; i < argc; ++i)
+		{
+			lua_pushstring(L, argv[i]);
+			lua_rawseti(L, -2, i - 1);
+		}
+		lua_setglobal(L, "arg");
+
+		error = lua_pcall(L, 0, LUA_MULTRET, 0);
+	}
+	if (error)
+	{
+		const char *error = lua_tostring(L, -1);
+		std::cout << "err: " << error << std::endl;
+	}
+
+	lua_close(L);
+
+	return error;
+}
